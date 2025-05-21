89a29d963a5da4d46c5075bbc4384e706eb78c76
Tăng Nguuuuuuu <59719049+omqn9696@users.noreply.github.com>
2025-05-21 16:32:00 +0700

    Update dialog.js

--- dialog.js
+++ dialog.js
@@ -1,16 +1,8 @@
-// ==UserScript==
-// @name         Pixels - Notify Pixel Tasks on Open & Changes
-// @namespace    http://tampermonkey.net/
-// @version      1.4
-// @description  Gửi thông báo pixel lần đầu mở bảng và theo dõi thay đổi tiếp tục, không thay đổi taskBoard.items
-// @author       Ốm.AI
-// @match        *://play.pixels.xyz/*
-// @grant        unsafeWindow
-// ==/UserScript==
-
 (function () {
     'use strict';
+
     const w = (typeof unsafeWindow !== 'undefined') ? unsafeWindow : window;
+
     const TELEGRAM_BOT_TOKEN = '7497533128:AAHnXF8-ASqxV3F23IpYsAW94Bl33I9nG7E';
     const TELEGRAM_CHAT_ID = '-1002593215567';
 
@@ -45,20 +37,16 @@
 
     const startWatchingTasks = () => {
         if (intervalId) return;
-        //console.log('[TASK TRACKER] Bắt đầu theo dõi taskBoard.items');
 
         intervalId = setInterval(() => {
-            const items = unsafeWindow?.pga?.store?.taskBoard?.items;
+            const items = w?.pga?.store?.taskBoard?.items;
             if (Array.isArray(items)) {
                 const currentJson = JSON.stringify(items);
                 if (currentJson !== lastJson) {
-                    // Chỉ gửi lần đầu mở nếu chưa gửi
                     if (!hasSentInitial) {
-                        //console.log('[TASK TRACKER] Lần đầu có dữ liệu, gửi các task pixel hiện tại');
                         processTasks(items);
                         hasSentInitial = true;
                     } else {
-                        // Lọc ra các task pixel mới hoặc khác so với trước
                         try {
                             const oldItems = JSON.parse(lastJson);
                             const oldIds = new Set(oldItems.map(t => t.taskItem.id + '_' + t.reward.quantity));
@@ -71,7 +59,6 @@
                                 }
                             }
                         } catch {
-                            // Nếu parse lỗi thì fallback gửi hết
                             processTasks(items);
                         }
                     }
@@ -87,7 +74,6 @@
             intervalId = null;
             lastJson = '';
             hasSentInitial = false;
-            //console.log('[TASK TRACKER] Ngừng theo dõi taskBoard.items');
         }
     };
 
@@ -106,6 +92,7 @@
 
     observeForSellPanel();
 })();
+
 
 (function() {
     'use strict';
